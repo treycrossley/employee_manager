8a26922cf7af47a27b0ba29cd80c889a0fdf4ecb
Trey Crossley <treycrossley@gmail.com>
2024-07-30 11:29:42 -0700

    item repository

---
+++ item-manager/src/main/java/com/example/item_manager/repository/ItemRepository.java
@@ -0,0 +1,9 @@
+package com.example.item_manager.repository;
+
+import com.example.item_manager.model.Item;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ItemRepository extends JpaRepository<Item, Long> {
+}
