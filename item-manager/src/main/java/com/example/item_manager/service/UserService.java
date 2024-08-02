6d03f4d57194d4e37dc9cece27bc22cfe59fd406
Trey Crossley <treycrossley@gmail.com>
2024-08-02 12:42:02 -0700

    Update UserService to use UserDetailService parent

--- item-manager/src/main/java/com/example/item_manager/service/UserService.java
+++ item-manager/src/main/java/com/example/item_manager/service/UserService.java
@@ -8,7 +8,7 @@ import javax.persistence.EntityNotFoundException;
 import java.util.Optional;
 
 @Service
-public class UserService {
+public class UserService implements UserDetailsService {
     @Autowired
     private UserRepository userRepository;
 
@@ -43,5 +43,16 @@ public class UserService {
         User user = getUserById(id);
         user.setPassword(passwordEncoder.encode(newPassword));
         userRepository.save(user);
+    }
+
+     @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with username: " + username));
+        return org.springframework.security.core.userdetails.User
+                .withUsername(user.getUsername())
+                .password(user.getPassword())
+                .authorities("USER") // You can set roles or authorities here
+                .build();
     }
 }
