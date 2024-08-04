85f207425f6ac158d2b052752d0c4325b3d7ed27
Eric Tapia-Garcia <gtapia.eric@hotmail.com>
2024-08-03 18:36:56 -0600

    added changes to files and fixed some typos

--- item-manager/src/main/java/com/example/item_manager/model/Employee.java
+++ item-manager/src/main/java/com/example/item_manager/model/Employee.java
@@ -44,7 +44,7 @@ public class Employee {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Employee employee = (Employee) o;
-        return id == employee.id &&
+        return Objects.equals(id, employee.id) &&
                 salary == employee.salary &&
                 Objects.equals(firstName, employee.firstName) &&
                 Objects.equals(lastName, employee.lastName) &&
