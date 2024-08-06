6a11a3229c5a4d95f46717eb03209a1cc79a6dae
Eric Tapia-Garcia <gtapia.eric@hotmail.com>
2024-08-06 13:57:50 -0600

    added delete user and get all users requests

--- item-manager/src/main/java/com/example/item_manager/service/UserService.java
+++ item-manager/src/main/java/com/example/item_manager/service/UserService.java
@@ -1,6 +1,5 @@
 package com.example.item_manager.service;
 
-import com.example.item_manager.model.Employee;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
